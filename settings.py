72a35b6815f412ec889f9ca43adb2f6b10fe8205
Dmitry <vasiutkin.dima@yandex.ru>
2022-01-18 19:13:49 +0300

    test

---
+++ settings.py
@@ -0,0 +1,3 @@
+BOT_TOKEN = '5031281217:AAGdZA15JZult6xoHlBH4rug3yxrgzFDC40'
+APP_URL = "https://t-heroku-db.herokuapp.com/" + BOT_TOKEN
+DB_URI = "postgres://lqtgtjziwzwcpw:1578d6e0feece6bf270d9f639135a0d980077cfb1fe80630181d9ebbf9625f9a@ec2-34-255-225-151.eu-west-1.compute.amazonaws.com:5432/dbghdvsrpontal"
